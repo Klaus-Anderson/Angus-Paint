8d92fa6810a1f932305a5f8e5f4a1996d9aae5b8
Harry Angus Cliff <harryacliff@gmail.com>
2016-03-09 19:38:03 -0500

    Created PaintView Class
    Created the PaintView Class, which will be used as the canvas which the
    user paints upon.

---
+++ MyApplication/app/src/main/java/games/angusgaming/anguspaint/PaintView.java
@@ -0,0 +1,15 @@
+package games.angusgaming.anguspaint;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Harry on 3/9/2016.
+ */
+public class PaintView extends View {
+
+    public PaintView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
